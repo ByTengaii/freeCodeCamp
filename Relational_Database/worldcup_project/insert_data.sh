d8ba356d5af5a4aa0e3c820b3a558adc74fb9c1f
Bytengaii <agirgurkan06@gmail.com>
2023-08-08 15:36:22 +0300

    worldcup project is done

--- Relational_Database/worldcup_project/insert_data.sh
+++ Relational_Database/worldcup_project/insert_data.sh
@@ -8,7 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-#echo $($PSQL "TRUNCATE games, teams")
+echo $($PSQL "TRUNCATE games, teams")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
